6f8be9f9e64aac6073ac7ab837e8c2f4b3e08c10
Ashish kumar <kumarashish9105@gmail.com>
2025-12-26 16:05:44 +0530

    make the card quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -57,7 +57,7 @@ products.forEach((product)=>{
 });
 
 
-    console.log(productsHTML);
+    
 
     document.querySelector('.js-product-grid').innerHTML=productsHTML;
 
@@ -81,6 +81,12 @@ products.forEach((product)=>{
                 });
             }
 
-            console.log(cart);
+            let cartQuantity =0;
+            cart.forEach((item)=>{
+              cartQuantity+=item.quantity;
+            });
+
+            
+            document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
         });
     });
